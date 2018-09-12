84d4358aa9fd775290b6dd0c64ca8508caaf0ced
wells <qishan.wu.1@lexisnexis.com>
2018-09-12 17:15:21 +0800

    refs

--- resources/views/home/common/team.blade.php
+++ resources/views/home/common/team.blade.php
@@ -5,12 +5,12 @@
         <div class="team">
             <div class="col-md-3 staff2">
                 <div class="view view-fifth wow fadeInLeft animated animated" data-wow-delay=".5s">
-                    <div class="mask1"><img src="/static/images/a.jpg" alt="image" class="img-responsive zoom-img" /></div>
+                    <div class="mask1"><img src="/static/images/huo1.jpg" alt="image" class="img-responsive zoom-img" /></div>
                     <div class="mask">
                         <a class="popup-with-zoom-anim" href="#small-dialog2"> <div class="info"><i class="glyphicon glyphicon-zoom-in"></i></div></a>
                         <div id="small-dialog2" class="mfp-hide">
                             <div class="pop_up2">
-                                <img src="/static/images/a.jpg" class="img-responsive" alt=""/>
+                                <img src="/static/images/huo1.jpg" class="img-responsive" alt=""/>
                                 <h3>About</h3>
                                 <p class="popup_desc">Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes </p>
                             </div>
@@ -28,12 +28,12 @@
             </div>
             <div class="col-md-3 staff2">
                 <div class="view view-fifth wow fadeInUp animated animated" data-wow-delay=".5s">
-                    <div class="mask1"><img src="/static/images/a.jpg" alt="image" class="img-responsive zoom-img" /></div>
+                    <div class="mask1"><img src="/static/images/huo1.jpg" alt="image" class="img-responsive zoom-img" /></div>
                     <div class="mask">
                         <a class="popup-with-zoom-anim" href="#small-dialog1"> <div class="info"><i class="glyphicon glyphicon-zoom-in"></i></div></a>
                         <div id="small-dialog1" class="mfp-hide">
                             <div class="pop_up2">
-                                <img src="/static/images/a.jpg" class="img-responsive" alt=""/>
+                                <img src="/static/images/huo1.jpg" class="img-responsive" alt=""/>
                                 <h3>About</h3>
                                 <p class="popup_desc">Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes </p>
                             </div>
@@ -51,12 +51,12 @@
             </div>
             <div class="col-md-3 staff2">
                 <div class="view view-fifth wow fadeInRight animated animated" data-wow-delay=".5s">
-                    <div class="mask1"><img src="/static/images/a.jpg" alt="image" class="img-responsive zoom-img" /></div>
+                    <div class="mask1"><img src="/static/images/huo1.jpg" alt="image" class="img-responsive zoom-img" /></div>
                     <div class="mask">
                         <a class="popup-with-zoom-anim" href="#small-dialog3"> <div class="info"><i class="glyphicon glyphicon-zoom-in"></i></div></a>
                         <div id="small-dialog3" class="mfp-hide">
                             <div class="pop_up2">
-                                <img src="/static/images/a.jpg" class="img-responsive" alt=""/>
+                                <img src="/static/images/huo1.jpg" class="img-responsive" alt=""/>
                                 <h3>About</h3>
                                 <p class="popup_desc">Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes </p>
                             </div>
@@ -75,12 +75,12 @@
 
             <div class="col-md-3 staff2">
                 <div class="view view-fifth wow fadeInRight animated animated" data-wow-delay=".5s">
-                    <div class="mask1"><img src="/static/images/a.jpg" alt="image" class="img-responsive zoom-img" /></div>
+                    <div class="mask1"><img src="/static/images/huo1.jpg" alt="image" class="img-responsive zoom-img" /></div>
                     <div class="mask">
                         <a class="popup-with-zoom-anim" href="#small-dialog3"> <div class="info"><i class="glyphicon glyphicon-zoom-in"></i></div></a>
                         <div id="small-dialog3" class="mfp-hide">
                             <div class="pop_up2">
-                                <img src="/static/images/a.jpg" class="img-responsive" alt=""/>
+                                <img src="/static/images/huo1.jpg" class="img-responsive" alt=""/>
                                 <h3>About</h3>
                                 <p class="popup_desc">Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes </p>
                             </div>
